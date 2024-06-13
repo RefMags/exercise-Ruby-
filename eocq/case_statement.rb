530816a3382a29c4e925a9e1e8a92b9df8f3c4e7
refilwe magwaneng <fyfymags@gmail.com>
2024-06-14 06:01:05 +0900

    flow control file

---
+++ eocq/case_statement.rb
@@ -0,0 +1,18 @@
+# a = 5
+
+# case a
+# when 5
+#   puts "a is 5"
+# when 6
+#   puts "a is 6"
+# else
+#   puts "a is neither 5 or 6"
+# end
+
+# if x = 5
+#   puts "how can this be true_"
+# else
+#   puts "it is not true"
+# end
+
+p 3 || 'foo'
