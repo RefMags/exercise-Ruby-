9459479120ea7d744c1352342e8e14b4a6f50204
refilwe magwaneng <fyfymags@gmail.com>
2024-07-05 06:08:52 +0900

    pass-by value basics done

--- prep/pass_by_objects.rb
+++ prep/pass_by_objects.rb
@@ -1,8 +1,16 @@
-def full_name(string)
-  string << " James"
-  # string + " " + "James"
+# def full_name(string)
+#   string << " James"
+#   # string + " " + "James"
+# end
+
+# name = "Sam"
+# full_name(name)
+# p name
+
+def display_total(num)
+	num + 30
 end
 
-name = "Sam"
-full_name(name)
-p name
+value = 6
+display_total(value)
+p value
